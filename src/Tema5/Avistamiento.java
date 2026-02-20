12b8b73d95a0c1b473b33fff0030270e269d212a
Nerea Lorite <nerlorala@alu.edu.gva.es>
2026-02-20 12:28:41 +0100

    5

--- src/Tema5/Avistamiento.java
+++ src/Tema5/Avistamiento.java
@@ -2,4 +2,21 @@ package Tema5;
 
 public class Avistamiento {
         int hora;
+
+        public Avistamiento() {
+                hora = 0;
+        }
+
+        public Avistamiento(int hora) {
+                this.hora = hora;
+        }
+
+        public int getHora(){
+                return hora;
+        }
+
+        public void setHora(int hora){
+                this.hora = hora;
+        }
+
 }
