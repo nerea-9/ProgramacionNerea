113528e78fe444fa19325f003d46a61b4be8266f
Nerea Lorite <nerlorala@alu.edu.gva.es>
2026-05-06 13:49:15 +0200

    5

--- src/RehacerTodoPrincipio/Tema1/Ejercicio3/Ejercicio2.java
+++ src/RehacerTodoPrincipio/Tema1/Ejercicio3/Ejercicio2.java
@@ -3,8 +3,6 @@ package RehacerTodoPrincipio.Tema1.Ejercicio3;
 public class Ejercicio2 {
     public static void main(String[] args) {
 
-
-
         for (int i = 1; i <= 200; i++) {
                 System.out.print(2 + i + "\n");
 
